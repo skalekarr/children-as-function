193217e40fb29eb81999bf25a9d82dd5b416f7ef
Sachin Kalekar <mr.sachinkalekar@gmail.com>
2019-06-04 13:55:27 +0530

    render children as function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Query extends Component {
+  state = {
+    loading: true,
+    error: {},
+    data: {}
+  }
+  componentDidMount() {
+    //do the apicall here
+    this.setState({ loading: false, data: { header: "may the force be with you" } });
+  }
+
+  render() {
+    const { state: props, props: { children } } = this;
+
+    // return the data here
+    // passing state as props here after renaming state to props
+    return children(props);
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Query>
+          {
+            ({ loading, error, data }) => {
+              if (loading) return <>Loading.......</>
+              if (Object.keys(error).length > 0) return <>Some Error Handler</>
+              if (data) return (
+                <>
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {data.header}
+                  </a>
+                </>
+              )
+            }
+          }
+        </Query>
       </header>
     </div>
   );
